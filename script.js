955403d1279c6f647190162c557bad2fff9e029c
dan <ferdanilo04@gmail.com>
2024-10-28 15:52:49 -0300

    new file:   .gitignore

---
+++ script.js
@@ -0,0 +1,18 @@
+window.Telegram.WebApp.onEvent('themeChanged', function() {
+    document.body.style.backgroundColor = window.Telegram.WebApp.colorScheme.bg_color;
+});
+
+window.Telegram.WebApp.ready(); // Prepara o WebApp para interações
+
+// Obter informações do usuário
+const userInfo = window.Telegram.WebApp.initDataUnsafe.user;
+document.getElementById('userInfo').innerHTML = `
+    <p>Nome: ${userInfo.first_name} ${userInfo.last_name || ''}</p>
+    <p>Username: ${userInfo.username}</p>
+`;
+
+// Enviar mensagem quando o botão for clicado
+document.getElementById('myButton').onclick = function() {
+    const message = "Olá, este é um teste do meu WebApp!";
+    window.Telegram.WebApp.sendMessage(userInfo.id, message);
+};
