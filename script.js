190e6cbb8da1f8da655c66a3a0bc58bf7ef5c954
dan <ferdanilo04@gmail.com>
2024-10-28 16:42:14 -0300

    first_name

--- script.js
+++ script.js
@@ -6,11 +6,14 @@ window.Telegram.WebApp.ready(); // Prepara o WebApp para interações
 
 // Obter informações do usuário
 const userInfo = window.Telegram.WebApp.initDataUnsafe.user;
-let w =window.Telegram.WebApp
+let w =window.Telegram.WebApp.initDataUnsafe.receiver
 console.log(w)
+
 document.getElementById('userInfo').innerHTML = `
     <p>Nome: ${userInfo.first_name} ${userInfo.last_name || ''}</p>
     <p>Username: ${userInfo.username}</p>
+    <p>chat: ${w.first_name}}</p>
+    
     
 `;
 
