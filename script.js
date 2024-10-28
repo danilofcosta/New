27e6ca05cf5ca21410a2d5c6d0cd4228693ca9a4
dan <ferdanilo04@gmail.com>
2024-10-28 16:27:30 -0300

    a

--- script.js
+++ script.js
@@ -6,9 +6,12 @@ window.Telegram.WebApp.ready(); // Prepara o WebApp para interações
 
 // Obter informações do usuário
 const userInfo = window.Telegram.WebApp.initDataUnsafe.user;
+let w =window.Telegram.WebApp
+console.log(w)
 document.getElementById('userInfo').innerHTML = `
     <p>Nome: ${userInfo.first_name} ${userInfo.last_name || ''}</p>
-    <p>Username: ${userInfo}</p>
+    <p>Username: ${userInfo.username}</p>
+    
 `;
 
 // Enviar mensagem quando o botão for clicado
