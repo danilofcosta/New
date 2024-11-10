826b09cf69371129999163aabbf78a08e9252789
dan <ferdanilo04@gmail.com>
2024-11-10 10:16:34 -0300

    console.log(receiver)

--- scripts/telegram.js
+++ scripts/telegram.js
@@ -1,11 +1,9 @@
 // Acessa a API do Telegram WebApp
 const telegramapi = window.Telegram.WebApp
 const user=telegramapi.initDataUnsafe.user
-const receiver=telegramapi.initDataUnsafe
+const receiver=telegramapi
 
 
 // Seleciona o elemento <h1> dentro do <header>
 document.querySelector("header h1").textContent = user.first_name +" "+user.last_name; // Define o texto do h1
-document.querySelector("header p").textContent =user.username // Define o texto do h1
-console.log(receiver)
-
+document.querySelector("header p").textContent =user.username 
